35e79465533e747f8ec41000c973e41619c6cc5b
Mohammadreza Rashidpour <rashidpour1995@gmail.com>
2025-02-22 09:22:45 +0330

    edit font-family css

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 (defined('ABSPATH')) || exit;
 
-define('ONI_VERSION', '2.0.7');
+define('ONI_VERSION', '2.0.8');
 
 define('ONI_PATH', get_template_directory() . "/");
 define('ONI_INCLUDES', ONI_PATH . 'includes/');
